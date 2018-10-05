d0f868430b521843d1c294c7bbfe1d999999a762
zejafo <zejafo@gmail.com>
2018-10-05 21:21:53 +0300

    readme update

--- Qwa.Angular/src/app/app.component.ts
+++ Qwa.Angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'zejafo.it';
+  name: String;
+
+  public setName(name: String) {
+    this.name = name;
+  }
 }
--- Qwa.Angular/src/app/app.module.ts
+++ Qwa.Angular/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
